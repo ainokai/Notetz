f2cfc86a0143ffe5fc1ab93e4f87efb7990621fe
EpiKin <74237483+EpiKin@users.noreply.github.com>
2021-05-11 11:10:21 +0300

    Lisätty alert -funktiot

--- app.js
+++ app.js
@@ -1,75 +1,115 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var noteCount = 0;
-    var activeNote = null;
-  
-    $('.color-box').click(function(){
-      var color = $(this).css('background-color');
-      $('notepad').css('background-color', color);
-      $('#title-field').css('background-color', color);
-      $('#body-field').css('background-color', color);
-    })
-  
-    $('#btn-save').click(function(){
-      var title = $('#title-field').val();
-      var body = $('#body-field').val();
-      if (title === '' && body === '') {
-        alert ('Please add a title or body to your note.');
-        return;
-      }
+  var noteCount = 0;
+  var activeNote = null;
+
+  $('.color-box').click(function () {
+    var color = $(this).css('background-color');
+    $('notepad').css('background-color', color);
+    $('#title-field').css('background-color', color);
+    $('#body-field').css('background-color', color);
+  })
+
+  $('#btn-save').click(function () {
+    var title = $('#title-field').val();
+    var body = $('#body-field').val();
+    if (title === '' && body === '') {
+      alert('Please add a title or body to your note.');
+      return;
+    }
+    var created = new Date();
+    var color = $('notepad').css('background-color');
+    var id = noteCount + 1;
+    if (activeNote) {
+      $('#' + activeNote)[0].children[0].innerHTML = title;
+      $('#' + activeNote)[0].children[1].innerHTML = created.toLocaleString("en-US");
+      $('#' + activeNote)[0].children[2].innerHTML = body;
+      $('#' + activeNote)[0].style.backgroundColor = color;
+      activeNote = null;
+      $('#edit-mode').removeClass('display').addClass('no-display');
+    } else {
       var created = new Date();
-      var color = $('notepad').css('background-color');
-      var id = noteCount + 1;
-      if (activeNote) {
-        $('#' + activeNote)[0].children[0].innerHTML = title;
-        $('#' + activeNote)[0].children[1].innerHTML = created.toLocaleString("en-US");
-        $('#' + activeNote)[0].children[2].innerHTML = body;
-        $('#' + activeNote)[0].style.backgroundColor = color;
-        activeNote = null;
-        $('#edit-mode').removeClass('display').addClass('no-display');
-      } else {
-        var created = new Date();
-        $('#listed').append('<div id="note' + id + '" style="background-color: ' + color + '"><div class="list-title">' + title + '</div> <div class="list-date">' + created.toLocaleString("en-US") + '</div> <div class="list-text">' + body + '</div> </div>');
-        noteCount++;
-      };
-      $('#title-field').val('');
-      $('#body-field').val('');
-      $('notepad').css('background-color', 'white');
-      $('#title-field').css('background-color', 'white');
-      $('#body-field').css('background-color', 'white');
-    });
-  
-    $('#btn-delete').click(function(){
-      if (activeNote) {
-        $('#' + activeNote)[0].remove();
-        activeNote = null;
-        $('#edit-mode').removeClass('display').addClass('no-display');
-      }
-        $('#title-field').val('');
-        $('#body-field').val('');
-        $('notepad').css('background-color', 'white');
-        $('#title-field').css('background-color', 'white');
-        $('#body-field').css('background-color', 'white');
-    });
-  
-    $('#listed').click(function(e){
-      var id = e.target.parentElement.id;
-      var color = e.target.parentElement.style.backgroundColor;
-      activeNote = id;
-      $('#edit-mode').removeClass('no-display').addClass('display');
-      var titleSel = $('#' + id)[0].children[0].innerHTML;
-      var bodySel = $('#' + id)[0].children[2].innerHTML;
-      $('#title-field').val(titleSel);
-      $('#body-field').val(bodySel);
-      $('notepad').css('background-color', color);
-      $('#title-field').css('background-color', color);
-      $('#body-field').css('background-color', color);
-    });
+      $('#listed').append('<div id="note' + id + '" style="background-color: ' + color + '"><div class="list-title">' + title + '</div> <div class="list-date">' + created.toLocaleString("en-US") + '</div> <div class="list-text">' + body + '</div> </div>');
+      noteCount++;
+    };
+    $('#title-field').val('');
+    $('#body-field').val('');
+    $('notepad').css('background-color', 'white');
+    $('#title-field').css('background-color', 'white');
+    $('#body-field').css('background-color', 'white');
+  });
 
-    $('#btn-saveChanges').click(function(){
-        alert ('All changes saved.');
-        return;
-      });
+  $('#btn-delete').click(function () {
+    if (activeNote) {
+      $('#' + activeNote)[0].remove();
+      activeNote = null;
+      $('#edit-mode').removeClass('display').addClass('no-display');
+    }
+    $('#title-field').val('');
+    $('#body-field').val('');
+    $('notepad').css('background-color', 'white');
+    $('#title-field').css('background-color', 'white');
+    $('#body-field').css('background-color', 'white');
+  });
+
+  $('#listed').click(function (e) {
+    var id = e.target.parentElement.id;
+    var color = e.target.parentElement.style.backgroundColor;
+    activeNote = id;
+    $('#edit-mode').removeClass('no-display').addClass('display');
+    var titleSel = $('#' + id)[0].children[0].innerHTML;
+    var bodySel = $('#' + id)[0].children[2].innerHTML;
+    $('#title-field').val(titleSel);
+    $('#body-field').val(bodySel);
+    $('notepad').css('background-color', color);
+    $('#title-field').css('background-color', color);
+    $('#body-field').css('background-color', color);
+  });
+
+  $('#btn-settingsSave').click(function () {
+    alert('All changes saved.');
+    return;
+  });
+
+  $('#btn-noOpenFunction').click(function () {
+    alert('Open file -function is not supported in this version.');
+    return;
+  });
+
+  $('#btn-noSaveFunction').click(function () {
+    alert('Save file -function is not supported in this version.');
+    return;
+  });
+
+  $('#btn-noSaveAsFunction').click(function () {
+    alert('Save file as -function is not supported in this version.');
+    return;
+  });
+
+  $('#btn-noDeleteFunction').click(function () {
+    alert('Delete file -function is not supported in this version.');
+    return;
+  });
+
+  $('#btn-noFindFunction').click(function () {
+    alert('Find -function is not supported in this version.');
+    return;
+  });
+
+  $('#btn-noUndoFunction').click(function () {
+    alert('Undo -function is not supported in this version.');
+    return;
+  });
+
+  $('#btn-noRedoFunction').click(function () {
+    alert('Redo -function is not supported in this version.');
+    return;
+  });
+
+  $('#btn-Guide').click(function () {
+    alert('Guide of the system is not included in this version.');
+    return;
+  });
 
 
- })
+})
